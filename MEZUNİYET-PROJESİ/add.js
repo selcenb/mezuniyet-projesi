e53c7f675580e37cb42e8fbc82d170a27cf58c31
Selcen Boran <164056944+selcenb@users.noreply.github.com>
2024-12-14 16:35:10 +0300

    Add files via upload

---
+++ MEZUNİYET-PROJESİ/add.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var text = document.querySelector('.slide-in-text');
+    text.classList.add('slide-in-text');
+};
+//? **************************************************************************************************************
+window.onload = function() {
+    var text = document.querySelector('.shadow-text');
+    text.classList.add('shadow-text');
+};
+
